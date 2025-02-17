91ac2a0be0f3a89163f8021ea840d8167e8b7559
Pedro <pedromf34@outlook.com>
2025-02-16 21:13:33 -0300

    feat: concrete table inheritance

---
+++ ObjectRelationalStructural/ConcreteTableInheritance/CriketerMapper.cs
@@ -0,0 +1,25 @@
+class CriketerMapper {
+    public override String TableName {
+        get{return "Criketers";}
+    }
+
+    public Cricketer Find(long id) {
+        return (Cricketer) AbstractFind(id);
+    }
+
+    protected override DomainObject CreateDomainObject() {
+        return new Cricketer();
+    }
+
+    protected override void Load(DomainObject obj, DataRow row) {
+        base.Load(obj, row);
+        Cricketer cricketer = (Cricketer) obj;
+        cricketer.battingAverage = (double)row["battingAverage"];
+    }
+
+    protected override void Save(DomainObject obj, DataRow row) {
+        base.Save(obj, row);
+        Cricketer cricketer = (Cricketer) obj;
+        row["battingAverage"] = cricketer.battingAverage;
+    }
+}
