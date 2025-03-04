666023e201a27daaa74f3d2b0653b79166c07add
Pedro <pedromf34@outlook.com>
2025-03-04 17:41:49 -0300

    feat: remote facade

---
+++ DistributionPatterns/RemoteFacade/JavaSessionBeanAsRemoteFacade/AlbumServiceBean.java
@@ -0,0 +1,33 @@
+package DistributionPatterns.RemoteFacade.JavaSessionBeanAsRemoteFacade;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+
+public class AlbumServiceBean implements AlbumService {
+    public AlbumDTO getAlbum(String id) throws RemoteException {
+        return new AlbumAssembler().writeDTO(Registry.findAlbum(id));
+    }
+
+    public String getAlbumXml(String id) throws RemoteException {
+        AlbumDTO dto = new AlbumAssembler().writeDTO(Registry.findAlbum(id));
+        return dto.toXmlString();
+    }
+
+    public void createAlbum(String id, AlbumDTO dto) throws RemoteException {
+        new AlbumAssembler().createAlbum(id, dto);
+    }
+
+    public void createAlbum(String id, String xml) throws RemoteException {
+        AlbumDTO dto = AlbumDTO.readXmlString(xml);
+        new AlbumAssembler().createAlbum(id, dto);
+    }
+
+    public void updateAlbum(String id, AlbumDTO dto) throws RemoteException {
+        new AlbumAssembler().updateAlbum(id, dto);
+    }
+
+    public void updateAlbum(String id, String xml) throws RemoteException {
+        AlbumDTO dto = AlbumDTO.readXmlString(xml);
+        new AlbumAssembler().updateAlbum(id, dto);
+    }
+}
